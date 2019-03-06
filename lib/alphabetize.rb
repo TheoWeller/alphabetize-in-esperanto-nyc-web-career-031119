d10f8fe03e473d541ed4dd7f11edb15c605ef506
Theo Weller <weller.theodor@gmail.com>
2019-03-06 20:09:13 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|string|ESPERANTO_ALPHABET}
+  arr.sort_by {|string|ESPERANTO_ALPHABET.split("")}
   #binding.pry
 end
